f968bb5940459657d235d0ce51a5ec3cfc360a82
AndrewZhuravlov <63732647+AndrewZhuravlov@users.noreply.github.com>
2020-08-02 13:00:56 +0300

    components

---
+++ to-do-list/src/components/Search.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Search = ()=>{
+    return(
+      <input placeholder = 'search' type="text"/> 
+    );
+  }
+  export default Search;
---
+++ to-do-list/src/components/ToDoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ const ToDoList = ()=>{
+    return(
+      <ul>
+        <li>Learn React</li>
+        <li>Build App</li>
+      </ul>
+    );
+  }
+
+  export default ToDoList;
